8c126177128b42d4fff07b95e5d83ba685e2dac5
Mina Youssef <MinaY@Microwize.com>
2021-10-15 19:28:06 +0200

    Initial Solution Structure

---
+++ SudorouxTest/SudorouxTest.BL/IServices/ICustomerService.cs
@@ -0,0 +1,10 @@
+﻿
+using SudorouxTest.BL.Dtos;
+
+namespace SudorouxTest.BL.IServices
+{
+    public interface ICustomerService
+    {
+        void SubmitCustomer(CustomerDto dto);
+    }
+}
---
+++ SudorouxTest/SudorouxTest.BL/Services/CustomerService.cs
@@ -0,0 +1,13 @@
+﻿using SudorouxTest.BL.Dtos;
+using SudorouxTest.BL.IServices;
+
+namespace SudorouxTest.BL.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        public void SubmitCustomer(CustomerDto dto)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
